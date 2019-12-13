382fe2e9cb0d3f771c21f6ed4d966fc63167f128
Joshua Steele <joshukraine@gmail.com>
2019-12-13 20:01:12 +0100

    Remove vim, add bashrc

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+git pull origin master;
+ln -nfs ~/.dotfiles/gemrc ~/.gemrc
+ln -nfs ~/.dotfiles/gitignore ~/.gitignore_global
+ln -nfs ~/.dotfiles/gitconfig ~/.gitconfig
+ln -nfs ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -nfs ~/.dotfiles/vim ~/.vim
+ln -nfs ~/.dotfiles/vimrc ~/.vimrc
+ln -nfs ~/.dotfiles/.zshrc ~/.zshrc
+ln -nfs ~/.dotfiles/.bashrc ~/.bashrc
+
+#if [ -n "$ZSH_VERSION" ]; then
+source ~/.zshrc;
+#elif [ -n "$BASH_VERSION" ]; then
+#  source ~/.bash_profile;
+#else
+#  echo 'unknown shell'
+#fi
